3162225dee0dffc8bbf6666b3a93306ee085623c
EREMOA <m011823932@edu.teu.ac.jp>
2019-11-27 08:02:49 +0900

    EnemyAttck(?)
    取り合えずの産物。プレイヤーができたた作り直す。

---
+++ Assets/Scripts/EnemyAttck.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttck : MonoBehaviour
+{
+	public GameObject bullet;
+	public GameObject player;
+	[SerializeField] float bulletSpeed;
+	private int attackCount = 0;
+
+    // Update is called once per frame
+    void Update()
+    {
+		attackCount += 1;
+
+		if (attackCount % 20 == 0 && attackCount <= 100)
+		{
+			BulletAttck();
+		}
+		else if (attackCount == 160)
+		{
+			attackCount = 0;
+		}
+    }
+	//これが攻撃の照準とかのやーつ
+	void BulletAttck()
+	{
+			GameObject enemyBullet = Instantiate(bullet, transform.position, Quaternion.identity);
+			Rigidbody bulletRd = enemyBullet.GetComponent<Rigidbody>();
+			enemyBullet.transform.LookAt(player.transform); 
+			bulletRd.velocity = enemyBullet.transform.forward.normalized * bulletSpeed;
+
+			Destroy(enemyBullet, 5.0f);
+	}
+}
