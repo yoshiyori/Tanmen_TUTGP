736e80e9d61794815495bd4f065884554141f29a
EREMOA <m011823932@edu.teu.ac.jp>
2019-12-18 19:25:03 +0900

    めぶからのやつをとりあえずくっつけてみた。

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    Transform cameraTrans;
+    [SerializeField] Transform playerTrans;
+    [SerializeField] Vector3 cameraVec; 
+    [SerializeField] Vector3 cameraRot;
+    void Start ()
+    {
+        cameraTrans = transform;
+        cameraTrans.rotation = Quaternion.Euler (cameraRot);
+    }
+    void LateUpdate ()
+    {
+        cameraTrans.position = Vector3.Lerp(cameraTrans.position, playerTrans.position + cameraVec, 1.0f * Time.deltaTime);
+    }
+}
