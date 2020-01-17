f727319f4a036d5c1654a4efe36e4a494306c7e6
tenki teramura <m0118187ab@edu.teu.ac.jp>
2020-01-17 18:41:18 +0900

    隕石を打ち出すのだけ作った
    コメントとかはまだつけてないから明日付ける

--- Assets/Scripts/MeteoriteShoter.cs
+++ Assets/Scripts/MeteoriteShoter.cs
@@ -4,18 +4,23 @@ using UnityEngine;
 
 public class MeteoriteShoter : MonoBehaviour
 {
-    public Rigidbody projectile;    //rigidbody型の変数を用意します
-    public Transform triggerPos;       //弾を撃つ場所の位置と回転、大きさを入れる変数shotPosを用意します
-    public float shotForce = 10f;  //浮動小数点型の変数shotForceを用意し、100fを入れます（弾を撃つ力））
+    public Rigidbody meteorite;    //rigidbody型の変数を用意します
+    public Transform triggerPos;
+    public float shotForce = 10f;  //浮動小数点型の変数shotForceを用意し、10fを入れます（弾を撃つ力））
 
     void OnTriggerEnter(Collider other)
     {
-        float y = 20.0f;
-        float z = Random.Range(-50.0f, 50.0f);
-        Rigidbody shot = Instantiate(projectile, Pos.position, shotPos.rotation) as Rigidbody;
+        if(other.gameObject.tag == "Player")
+        {
+            float y = 20.0f;
+            float z = Random.Range(-15.0f, 15.0f);
+            Vector3 position = new Vector3(triggerPos.position.x, y, z);
+            Rigidbody shot = Instantiate(meteorite, position, triggerPos.rotation) as Rigidbody;
+            shot.AddForce(-triggerPos.right * shotForce, ForceMode.Impulse);
+        }
     }
 
-    void Update()
+    /*void Update()
     {
         if (Input.GetButtonUp("Fire1"))     //もし”Fire1”（左Ctrlキー）が押されたなら・・
         {
@@ -24,5 +29,5 @@ public class MeteoriteShoter : MonoBehaviour
             //変数shotで作られた複製にAddForceで物理的な力（Impulse衝撃力）をz軸方向（forward）に加えます
             shot.AddForce(shotPos.forward * shotForce, ForceMode.Impulse);
         }
-    }
+    }*/
 }
