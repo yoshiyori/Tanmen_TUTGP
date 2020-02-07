c00149d755b4d4161fe444661396083998da4e80
tenki teramura <m0118187ab@edu.teu.ac.jp>
2020-02-07 23:06:02 +0900

    隕石を打ち出すやつ一応完成
    コメントもちゃんとつけました。隕石自体を作った後にもう少し改良するかも

--- Assets/Scripts/MeteoriteShoter.cs
+++ Assets/Scripts/MeteoriteShoter.cs
@@ -4,30 +4,21 @@ using UnityEngine;
 
 public class MeteoriteShoter : MonoBehaviour
 {
-    public Rigidbody meteorite;    //rigidbody型の変数を用意します
-    public Transform triggerPos;
-    public float shotForce = 10f;  //浮動小数点型の変数shotForceを用意し、10fを入れます（弾を撃つ力））
+    public Rigidbody meteorite;  //打ち出す隕石を入れる変数
+    public Transform triggerPos;  //triggerの位置を入れる変数
+    [SerializeField] float shotForce = 10f;  //隕石を打ち出す力の数値
 
     void OnTriggerEnter(Collider other)
     {
         if(other.gameObject.tag == "Player")
         {
-            float y = 20.0f;
-            float z = Random.Range(-15.0f, 15.0f);
-            Vector3 position = new Vector3(triggerPos.position.x, y, z);
-            Rigidbody shot = Instantiate(meteorite, position, triggerPos.rotation) as Rigidbody;
-            shot.AddForce(-triggerPos.right * shotForce, ForceMode.Impulse);
+            //隕石を打ち出す場所を決める
+            float y = 20.0f;　//高さ
+            float z = Random.Range(-15.0f, 15.0f);　//横の位置はランダム（現在は道幅いっぱい）
+            Vector3 position = new Vector3(triggerPos.position.x, y, z);　//打ち出す場所を入れる変数
+
+            Rigidbody shot = Instantiate(meteorite, position, triggerPos.rotation) as Rigidbody;　//隕石生成
+            shot.AddForce(-triggerPos.right * shotForce, ForceMode.Impulse);　//隕石発射
         }
     }
-
-    /*void Update()
-    {
-        if (Input.GetButtonUp("Fire1"))     //もし”Fire1”（左Ctrlキー）が押されたなら・・
-        {
-            //Rigidbody型の変数shotを用意し、Instantiateで（prefab、位置、回転）を指定して複製したものを入れます
-            Rigidbody shot = Instantiate(projectile, shotPos.position, shotPos.rotation) as Rigidbody;
-            //変数shotで作られた複製にAddForceで物理的な力（Impulse衝撃力）をz軸方向（forward）に加えます
-            shot.AddForce(shotPos.forward * shotForce, ForceMode.Impulse);
-        }
-    }*/
 }
