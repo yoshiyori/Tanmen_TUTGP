5d3214936177c8c70add49d0ecde1dca7fe7c026
ERENOA <m011823932@edu.teu.ac.jp>
2020-03-09 14:40:49 +0900

    とりあえずどうぞ

--- Assets/Scripts/InertiaPlayer.cs
+++ Assets/Scripts/InertiaPlayer.cs
@@ -28,6 +28,7 @@ public class InertiaPlayer : MonoBehaviour
 	private bool mudTrigger;
 	public bool junp;
 	private bool willieFlg;
+	public bool turnTipe;
 	private Vector3 nowSpeed;
 	private Vector3 oldSpeed;
 	private Vector3 pos;
@@ -53,7 +54,108 @@ public class InertiaPlayer : MonoBehaviour
 	// Update is called once per frame
 	void Update()
     {
-		/*	気にしたら負け
+		
+		mudTrigger = mud.GetComponent<Obstacle>().triggerObsFlag;
+		//junp = junpFlag.GetComponent<JunpJudg>().nowJunpFlag;							//サウンド変更部分 2/5
+		willieFlg = objectPlayer.GetComponent<PlayerDirecting>().willieFlg;
+		
+		nowSpeed = rigid.velocity;
+		pos = this.gameObject.transform.localEulerAngles;
+
+		//ここで速度とかの制御
+
+		if (mudTrigger == false && rigid.velocity.magnitude < maxSpeed)
+		{
+			rigid.AddRelativeForce(-accelSpeed, 0, 0);
+		}
+		else if (willieFlg == false)
+		{
+			if (mudTrigger == true)
+			{
+				rigid.velocity = new Vector3(-1, 0, 0);
+
+			}
+		}
+		else if (maxSpeed < -nowSpeed.x)
+		{
+			nowSpeed.x = oldSpeed.x - 10;
+
+		}
+		
+		if (Input.GetKey(KeyCode.DownArrow) && rigid.velocity.x<0.1)
+		{
+			rigid.AddRelativeForce(brakeSpeed,0, 0);
+			playerSound.Play("Break");														//サウンド追加分 3/5
+		}
+		if (rigid.velocity.x > 0)
+		{
+			rigid.velocity = Vector3.zero;
+		}
+
+		//回転
+		turnPlayer();
+
+		junpFlag.GetComponent<JunpJudg>().JunpPlayer();
+
+		//サウンド追加分 4/5
+		if(succesRollingJump){
+			playerSound.Play("Rolling");
+			succesRollingJump = false;
+		}
+
+		if((nowSpeed.magnitude > 1f) && !runningSound.status.ToString().Equals("Playing")){
+			runningSound.Play();
+		}
+		if(nowSpeed.magnitude <= 1f){
+			runningSound.Stop();
+		}
+		//サウンド追加分 4/5 終了	
+			
+
+		//確認用
+		if (Input.GetKey(KeyCode.Z))
+		{
+			Debug.Log(rigid.velocity.magnitude);
+
+		}
+		
+		oldSpeed = nowSpeed;
+
+
+	}
+	
+	//サウンド追加分 5/5
+	void OnCollisionEnter(Collision other)
+	{
+		//Debug.Log(other.gameObject.name);
+		if(other.gameObject.tag.Equals("Lode") && junp)
+		{
+			playerSound.Play("Landing");
+			junp = false;
+		}
+	}
+	void turnPlayer()
+	{
+		if (Input.GetKey(KeyCode.RightArrow))
+		{
+			this.gameObject.transform.Rotate(new Vector3(0, rotaSpeed, 0));
+			if (turnTipe == true)
+			{
+				rigid.velocity = Quaternion.Euler(0, rotaSpeed, 0) * rigid.velocity;
+			}
+		}
+		if (Input.GetKey(KeyCode.LeftArrow))
+		{
+			this.gameObject.transform.Rotate(new Vector3(0, -rotaSpeed, 0));
+			if (turnTipe == true)
+			{
+				rigid.velocity = Quaternion.Euler(0, -rotaSpeed, 0) * rigid.velocity;
+			}
+		}
+
+	}
+}
+/*	気にしたら負け
 		 	if (OneTime == true)
 			{
 				Direction = Quaternion.Euler(Pold) * Vector3.right;
@@ -99,95 +201,3 @@ public class InertiaPlayer : MonoBehaviour
 				Accel = Accel + 0.03f;
 			}
 			*/
-		mudTrigger = mud.GetComponent<Obstacle>().triggerObsFlag;
-		//junp = junpFlag.GetComponent<JunpJudg>().nowJunpFlag;							//サウンド変更部分 2/5
-		willieFlg = objectPlayer.GetComponent<PlayerDirecting>().willieFlg;
-		
-		nowSpeed = rigid.velocity;
-		pos = this.gameObject.transform.localEulerAngles;
-
-		//ここで速度とかの制御
-
-		if (mudTrigger == false && rigid.velocity.magnitude < maxSpeed)
-		{
-			rigid.AddRelativeForce(-accelSpeed, 0, 0);
-		}
-		else if (willieFlg == false)
-		{
-			if (mudTrigger == true)
-			{
-				rigid.velocity = new Vector3(-1, 0, 0);
-
-			}
-		}
-		else if (maxSpeed < -nowSpeed.x)
-		{
-			nowSpeed.x = oldSpeed.x - 10;
-
-		}
-		
-		if (Input.GetKey(KeyCode.DownArrow) && rigid.velocity.x<0.1)
-		{
-			rigid.AddRelativeForce(brakeSpeed,0, 0);
-			playerSound.Play("Break");														//サウンド追加分 3/5
-		}
-		if (rigid.velocity.x > 0)
-		{
-			rigid.velocity = Vector3.zero;
-		}		
-
-		//回転
-
-		if (Input.GetKey(KeyCode.RightArrow))
-		{
-			this.gameObject.transform.Rotate(new Vector3(0, rotaSpeed, 0));
-			//rigid.velocity = Quaternion.Euler(0, rotaSpeed, 0)* rigid.velocity;
-			
-		}
-		if (Input.GetKey(KeyCode.LeftArrow))
-		{
-			this.gameObject.transform.Rotate(new Vector3(0, -rotaSpeed, 0));
-			//rigid.velocity = Quaternion.Euler(0, -rotaSpeed, 0) * rigid.velocity;
-		}
-
-		junpFlag.GetComponent<JunpJudg>().JunpPlayer();
-
-		//サウンド追加分 4/5
-		if(succesRollingJump){
-			playerSound.Play("Rolling");
-			succesRollingJump = false;
-		}
-
-		if((nowSpeed.magnitude > 1f) && !runningSound.status.ToString().Equals("Playing")){
-			runningSound.Play();
-		}
-		if(nowSpeed.magnitude <= 1f){
-			runningSound.Stop();
-		}
-		//サウンド追加分 4/5 終了	
-			
-
-		//確認用
-		if (Input.GetKey(KeyCode.Z))
-		{
-			Debug.Log(rigid.velocity.magnitude);
-
-		}
-		
-		oldSpeed = nowSpeed;
-
-
-	}
-	
-	//サウンド追加分 5/5
-	void OnCollisionEnter(Collision other)
-	{
-		//Debug.Log(other.gameObject.name);
-		if(other.gameObject.tag.Equals("Lode") && junp)
-		{
-			playerSound.Play("Landing");
-			junp = false;
-		}
-	}
-	
-}
