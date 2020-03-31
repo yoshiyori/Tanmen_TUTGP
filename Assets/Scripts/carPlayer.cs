7625b89822592f10cec6b372fe178716ede7f0c3
ERENOA <m011823932@edu.teu.ac.jp>
2020-03-31 22:31:12 +0900

    deketa

--- Assets/Scripts/carPlayer.cs
+++ Assets/Scripts/carPlayer.cs
@@ -4,28 +4,27 @@ using UnityEngine;
 
 public class carPlayer : MonoBehaviour
 {
-    [Range(-1.0f, +1.0f)]
-    public float steering = 0.0f;
-  
-    public float engineTorqu;
 
+    [Range(-2.0f, +2.0f)]
+    public float steering = 0.0f;
+    public float engineTorqu;
     public float breakTorqu;
     public float maxAngle;
     public float maxTorqu;
     public float maxBreakTorqe;
-
+    private bool check;
     public WheelCollider[] FrontWheel;
     public WheelCollider[] RearWheel;
     // Start is called before the first frame update
     void Start()
     {
-        
+       
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        check = false;
         // ハンドルを切る
         float angleWheel = maxAngle * steering;
         foreach (var wheel in FrontWheel)
@@ -48,23 +47,32 @@ public class carPlayer : MonoBehaviour
         foreach (var wheel in FrontWheel)
         {
             wheel.brakeTorque = breakTorqu2;
+          
         }
         foreach (var wheel in RearWheel)
         {
             wheel.brakeTorque = breakTorqu2;
+          
         }
 
-        if (Input.GetKey(KeyCode.RightArrow) && steering < 1.0)
+        if (Input.GetKey(KeyCode.RightArrow) && steering < 2.0)
         {
             steering = steering + 0.1f;
+            check = true;
         }
-        else if (Input.GetKey(KeyCode.LeftArrow) && steering > -1.0)
+        else if (Input.GetKey(KeyCode.LeftArrow) && steering > -2.0)
         {
             steering = steering + -0.1f;
+            check = true;
         }
-        else
+        else if (Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow))
+        {
+            check = true;
+        }
+        else if(check == false)
         {
             steering = 0;
+    
         }
     }
 }
