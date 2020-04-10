9a1c4a5f82c004b28641d46e2c844596919f79af
yoshiyori <m0118278c9@edu.teu.ac.jp>
2020-04-10 19:43:59 +0900

    ランキングに関する値のセーブ機能のめどがついた。

---
+++ Assets/Scripts/RankingManager.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class RankingManager : MonoBehaviour
+{
+    SaveManager sm;
+
+    private string[] rankingPlayerName;
+    private float[] rankingPlayerTime;
+
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SaveManager : MonoBehaviour
+{
+    string filePath;
+    RankingSaveData save;
+
+    void Awake()
+    {
+        filePath = "Assets/SaveData" + "/" + ".rankingsavedata.json";
+        save = new RankingSaveData();
+    }
+
+    public void Save()
+    {
+        string json = JsonUtility.ToJson(save);
+
+        StreamWriter streamWriter = new StreamWriter(filePath);
+        streamWriter.Write(json);
+        streamWriter.Flush();
+        streamWriter.Close();
+    }
+
+    public void Load()
+    {
+        if (File.Exists(filePath))
+        {
+            StreamReader streamReader;
+            streamReader = new StreamReader(filePath);
+            string data = streamReader.ReadToEnd();
+            streamReader.Close();
+
+            save = JsonUtility.FromJson<RankingSaveData>(data);
+        }
+    }
+
+}
