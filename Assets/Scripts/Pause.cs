4e4e4b7069c242353bfc353636c1cd6092cdf6b7
tenki teramura <m0118187ab@edu.teu.ac.jp>
2020-04-15 15:16:01 +0900

    ブランチ作り直し
    developの変更を前のブランチが取り込めなくなってしまったので作り直した

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseUI;
+    [SerializeField] private CueManager cueManager;
+
+    void Reset()
+    {
+        cueManager = (CueManager)FindObjectOfType(typeof(CueManager));
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("q"))
+        {
+            pauseUI.SetActive(!pauseUI.activeSelf);
+
+            if (pauseUI.activeSelf)
+            {
+                Time.timeScale = 0f;
+                //キューシート「Game_SE」に属するキューの再生を全て一時停止する
+                //一時停止中に再度、再生要求があると再生を再開するため注意 (後々修正するかも)
+                cueManager.PauseCueSheet("Game_SE");
+            }
+            else
+            {
+                Time.timeScale = 1f;
+                //キューシート「Game_SE」に属するキューの再生を全て再開するする
+                cueManager.RestartCueSheet("Game_SE");
+            }
+        }
+    }
+}
