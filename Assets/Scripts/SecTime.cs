984b383baf8040efbdf0ea176e0f715de7b71560
tenki teramura <m0118187ab@edu.teu.ac.jp>
2020-05-03 15:28:06 +0900

    タイム計測機能ひとまず完成
    タイム計測に必要なものはprefab化してあります
    スタート前処理やゴール処理が追加されたら順次追加

---
+++ Assets/Scripts/SecTime.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SecTime : MonoBehaviour
+{
+    //TimeManagerスクリプトから各種変数を参照する
+    GameObject timeManager;
+    TimeManager timeManagerScript;
+
+    float secTime = 0f; //セクタータイム計算用変数
+
+    //タイム表示関係
+    int minutes, seconds, mseconds;
+    public GameObject secTimeText; //セクタータイム表示テキスト
+
+    private void Start()
+    {
+        //TimeMAnagerスクリプトから参照するための準備
+        timeManager = GameObject.Find("TimeManager");
+        timeManagerScript = timeManager.GetComponent<TimeManager>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.tag == "Player")
+        {
+            secTime = timeManagerScript.totalTime - timeManagerScript.oldSecTime; //全体のタイムからそれまでのセクタータイムの合計を引く
+
+            //テキスト表示用処理
+            minutes = Mathf.FloorToInt(secTime / 60f);
+            seconds = Mathf.FloorToInt(secTime % 60f);
+            mseconds = Mathf.FloorToInt((secTime % 60f - seconds) * 1000);
+            secTimeText.GetComponent<Text>().text = string.Format("sec{0:0}　{1:00}:{2:00}.{3:000}", timeManagerScript.secNumber, minutes, seconds, mseconds);
+            secTimeText.SetActive(!secTimeText.activeSelf);
+
+            //セクタータイムを前のセクタータイムの合計値に足す
+            timeManagerScript.oldSecTime += secTime;
+            timeManagerScript.secNumber++; //セクターの数字が合うように1足す
+
+            //ゴールフラグ処理
+            if(timeManagerScript.secNumber == 5)
+            {
+                timeManagerScript.goalFlag = true;
+            }
+        }
+    }
+
+}
--- Assets/Scripts/TimeManager.cs
+++ Assets/Scripts/TimeManager.cs
@@ -5,26 +5,53 @@ using UnityEngine.UI;
 
 public class TimeManager : MonoBehaviour
 {
-    public float totalTime; //全体のタイム計測
-    /*public GameObject[] secEndPosition;
-    float secTime;
-    float[] secEndTime;
-    private int secNumber;*/
+
+    bool startFlag = true; //スタート前演出追加した時用。今はずっとtrue
+    [System.NonSerialized] public bool goalFlag = false;　//ゴール判断用
+    [System.NonSerialized] public float totalTime; //全体のタイム計測
+
+    //タイム表示関係
+    int minutes, seconds, mseconds;
+    public Text totalTimeText; //全体のタイム表示テキスト
+
+    [System.NonSerialized] public int secNumber; //セクター表記用変数
+    [System.NonSerialized] public float oldSecTime; //セクター計算用変数
 
     void Start()
     {
-        totalTime = 0f;
-        //secNumber = 0;
-        //secEndTime = new float[secEndPosition.Length];
+        //変数系初期化
+        totalTime = 0.0f;
+        oldSecTime = 0.0f;
+        secNumber = 1;
+        minutes = 0;
+        seconds = 0;
+        mseconds = 0;
     }
 
     void Update()
     {
-        totalTime += Time.deltaTime;
-        //Debug.Log("Time:" + totalTime);
+        if (startFlag == false)
+        {
+            //スタート前処理を入れる時用（たぶんここには入れないだろうけど念のため）
+        }
+        else if (startFlag == true)
+        {
+            if(goalFlag == false)
+            {
+                totalTime += Time.deltaTime; //ここでタイム計測
 
-        //secTime += Time.deltaTime;
-        //Debug.Log("secTime:" + secTime);
+                //テキスト表示用処理
+                minutes = Mathf.FloorToInt(totalTime / 60f);
+                seconds = Mathf.FloorToInt(totalTime % 60f);
+                mseconds = Mathf.FloorToInt((totalTime % 60f - seconds) * 1000);
+                totalTimeText.text = string.Format("Time　{0:00}:{1:00}.{2:000}", minutes, seconds, mseconds);
+
+            }
+            if(goalFlag == true)
+            {
+                //ゴールした時の処理を入れる（現状何もなし）
+            }
+        }
     }
 
 }
